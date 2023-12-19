4f182f5be8436df380bb9c025d9c250fb6cfb09f
AtelierMirai <mirai@AtelierMirainoMac-Studio.local>
2023-12-19 17:20:43 +0900

    chapter 06

--- rpg/rpg01.rb
+++ rpg/rpg01.rb
@@ -1,5 +1,7 @@
-system "tput civis"
-system "clear"
-
 puts "遙かなる未来への旅立ち"
 puts "--- 失われし伝説を求めて ---"
+
+puts "スライムが現れた"
+puts "勇者の攻撃"
+puts "スライムに10ポイントのダメージを与えた"
+puts "スライムを倒した"
--- rpg/rpg02.rb
+++ rpg/rpg02.rb
@@ -1,10 +1,11 @@
-system "tput civis"
-system "clear"
-
 puts "遙かなる未来への旅立ち"
 puts "--- 失われし伝説を求めて ---"
 
 puts "スライムが現れた"
+gets
 puts "勇者の攻撃"
+gets
 puts "スライムに10ポイントのダメージを与えた"
+gets
 puts "スライムを倒した"
+gets
